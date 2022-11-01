07a070ac0f6aea2827f070a64f7a70a7ef217069
eliotheinrich <eliot.heinrich@gmail.com>
2022-11-01 16:22:51 -0400

    Added plot_entropy.py

--- plot_entropy.py
+++ plot_entropy.py
@@ -1,40 +1,106 @@
+from cmath import isclose
 import numpy as np
 import matplotlib.pyplot as plt
-import re
 
-def remove_chars(s, chars):
-	for char in chars:
-		s = s.replace(char, '')
-	return s
+class DataFrame:
+	def __init__(self, s):
+		self.data = {}
+		items = s.split('\t')
+		for item in items:
+			key = item.split(':')[0].strip()
+			vals = item.split(':')[1].strip()
+			vals = [float(x) for x in vals.split(' ')]
+			if len(vals) == 1:
+				self.data[key] = vals[0]
+			else:
+				self.data[key] = vals
+		
+	def __getitem__(self, key):
+		return self.data[key]
+
+class DataFrameCollection:
+	def __init__(self):
+		self.dfs = []
+
+	def add_dataframe(self, df):
+		self.dfs.append(df)
+
+	def get_property_with_id(self, key, id):
+		return self.dfs[id][key]
+
+	def get_property(self, key):
+		val = []
+		for df in self.dfs:
+			val.append(df[key])
+		return np.array(val)
+	
+	def query_key(self, key, val):
+		new_dfc = DataFrameCollection()
+		for df in self.dfs:
+			if np.isclose(df[key], val):
+				new_dfc.add_dataframe(df)
+		return new_dfc
 
 def load_data(filename):
-	p = []
-	LA = []
-	entropy = []
-	entropy_err = []
-
+	data = DataFrameCollection()
 	with open('data.txt', 'r') as f:
 		lines = [i.strip() for i in f.readlines()]
 		num_lines = len(lines)
-		for i in range(num_lines//3):
-			p.append(float(lines[3*i]))
-			LA = remove_chars(lines[3*i + 1], [']','[',',']).split(' ')
-			d = [remove_chars(s, ['(',')']) for s in re.findall('\[[^\]]*\]|\([^\)]*\)|\"[^\"]*\"|\S+', remove_chars(lines[3*i + 2], [']', '[',',']))]
-			entropy.append([float(i.split(' ')[0]) for i in d])
-			entropy_err.append([float(i.split(' ')[1]) for i in d])
+		for line in lines:
+			data.add_dataframe(DataFrame(line))
+	
+	return data
 
-	LA = [int(i) for i in LA]
-	return p, LA, entropy, entropy_err
+def plot_run(data: DataFrameCollection, run_id: int, average_interval: int = 1, ax = None):
+	if ax is None:
+		ax = plt.gca()
 
-def plot_data(ax, p, LA, entropy, entropy_err):
-	for n,prob in enumerate(p):
-		ax.errorbar(LA, entropy[n], entropy_err[n], label=f'p = {prob}')
-	ax.set_xlabel(r'$L_A$', fontsize=15)
-	ax.set_ylabel(r'$\bar{S^2_A}$', fontsize=15)
-	ax.legend()
+	entropy = data.get_property_with_id("entropy", run_id)
+	if average_interval != 1:
+		times = []
+		entropy_avg = []
+		for i in range(len(entropy)//average_interval):
+			times.append(i*average_interval)
+			entropy_avg.append(np.mean(entropy[i*average_interval:(i+1)*average_interval]))
+	else:
+		entropy_avg = entropy
 
-fig, ax = plt.subplots()
-plot_data(ax, *load_data("data.txt"))
+	ax.plot(entropy_avg)
+	ax.set_xlabel(r'$t$', fontsize=16)
+	ax.set_ylabel(r'$S_A^2$', fontsize=16)
 
-plt.show()
+def plot_all_data(data: DataFrameCollection, steady_state: int = 0, ax = None):
+	if ax is None:
+		ax = plt.gca()
 
+	unique_p = sorted(list(set(data.get_property('p'))))
+	unique_LA = sorted(list(set(data.get_property('LA'))))
+	entropy_avg = np.zeros((len(unique_p), len(unique_LA)))
+	for df in data.dfs:
+		i = unique_p.index(df['p'])
+		j = unique_LA.index(df['LA'])
+
+		entropy_avg[i][j] = np.mean(df['entropy'][steady_state:])
+
+	colors = ['blue', 'orange', 'yellow', 'purple', 'green']
+	for n,p in enumerate(unique_p):
+		ax.plot(unique_LA, entropy_avg[n], linewidth=1.5, marker='*', color=colors[n], label=f'p = {p}')
+
+	ax.legend(fontsize=16)
+	ax.set_xlabel(r'$L_A$', fontsize=16)
+	ax.set_ylabel(r'$\overline{S_A^{(2)}}$', fontsize=16)
+
+data = load_data('data.txt')
+
+unique_p = sorted(list(set(data.get_property('p'))))
+for p in unique_p:
+	p_data = data.query_key('p', 0.08)
+
+	fig, ax = plt.subplots()
+	for i in range(len(p_data.dfs)):
+		plot_run(data, i, 3000, ax)
+	ax.set_title(f'p = {p}', fontsize=16)
+	plt.show()
+
+#plot_run(data, 50, 3000)
+#plot_all_data(data)
