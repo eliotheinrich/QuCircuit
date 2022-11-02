2982ea6243074b93ac7846eeedf0991d8d2947b2
Eliot A Heinrich <heinriea@l001.cm.cluster>
2022-11-02 14:04:52 -0400

    Changed to require entropy measurement frequency

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,15 @@ use quantum_circuit::quantum_chp_state::QuantumCHPState;
 use std::time::Instant;
 
 fn main() {
-    let num_threads = 4;
+    let num_threads = 48;
     rayon::ThreadPoolBuilder::new().num_threads(num_threads).build_global().unwrap();
 
     
     let now = Instant::now();
     
-    take_data::<QuantumCHPState>(40, 2, 
-                                10, 100, 
-                                String::from("data.txt") );
+    take_data::<QuantumCHPState>(400, 25, 
+                                100000, 5,
+                                String::from("data2.txt") );
 
     let elapsed = now.elapsed();
     println!("Time: {:.2?}", elapsed);
