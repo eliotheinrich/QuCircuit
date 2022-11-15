cc7342d2d0fa22b0999c4963176d3857b9be7b72
Eliot A Heinrich <heinriea@l001.cm.cluster>
2022-11-15 14:10:03 -0500

    Added num_threads to CLI interface

--- src/main.rs
+++ src/main.rs
@@ -24,5 +24,7 @@ fn compute_entropy_run(num_threads: usize, config_filename: &String) {
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
-    compute_entropy_run(1, &args[1])
+    let num_threads = args[1].parse::<usize>().unwrap();
+    let cfg_filename = &args[2];
+    compute_entropy_run(num_threads, cfg_filename)
 }
